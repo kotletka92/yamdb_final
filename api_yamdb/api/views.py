332eeb21d942ca4b790bb8ac04c9ae70cefe3fff
Антон Филатов <filatovap2013@yandex.ru>
2022-12-19 04:04:40 +0300

    first project1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.pagination import (LimitOffsetPagination,
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
+from api.validators import validate_year
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from api.validators import validate_year
 
 
 class User(AbstractUser):
